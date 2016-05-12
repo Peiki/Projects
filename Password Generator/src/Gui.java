114ab91b9370c53c6671778553ba0c63ea360229
Peiki <peikimo@hotmail.it>
2016-05-12 18:45:05 +0200

    Added Digit and SpecialCharacter

--- Password Generator/src/Gui.java
+++ Password Generator/src/Gui.java
@@ -64,17 +64,22 @@ public class Gui{
 		b.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try{
+					if(Integer.parseInt(tf.getText())<0 || Integer.parseInt(tf_1.getText())<0 || Integer.parseInt(tf_2.getText())<0) throw new IllegalArgumentException();
 					if(Integer.parseInt(tf.getText())+Integer.parseInt(tf_1.getText())+Integer.parseInt(tf_2.getText())>=8 && Integer.parseInt(tf_3.getText())>0){
 						progressBar.setValue(100);
-						String s=generate();
+						int x[]=new int[4];
+						x[0]=Integer.parseInt(tf_3.getText());
+						x[1]=Integer.parseInt(tf.getText());
+						x[2]=Integer.parseInt(tf_1.getText());
+						x[3]=Integer.parseInt(tf_2.getText());
+						String s=generate(x);
 						if(t==false){
 							ta=new TextArea(f.getX(),f.getY());
-							ta.setText("Ciao");
+							ta.setText(s);
 							t=true;
 						}
 						else{
-							ta.setText("");
-							//TODOs
+							ta.setText(s);
 						}
 					}
 					else if(Integer.parseInt(tf_3.getText())<=0)
@@ -83,6 +88,9 @@ public class Gui{
 						JOptionPane.showMessageDialog(null, "Utilizzare almeno 8 caratteri","Error",JOptionPane.ERROR_MESSAGE);
 				}
 				catch(NumberFormatException e1){
+					JOptionPane.showMessageDialog(null, "Errore nell'inserimento","Error",JOptionPane.ERROR_MESSAGE);
+				}
+				catch(IllegalArgumentException e2){
 					JOptionPane.showMessageDialog(null, "Errore nell'inserimento","Error",JOptionPane.ERROR_MESSAGE);
 				}
 			}
@@ -118,8 +126,62 @@ public class Gui{
 		f.getContentPane().add(lblm);
 		f.getContentPane().add(chckbxNewCheckBox);
 	}
-	public String generate(){
-		return "";
-		//TODOs
+	public String generate(int x[]){
+		String s="";
+		int r,y[]=new int[4];
+		while(x[0]!=0){
+			copy(x,y);
+			while(!control(y)){
+				r=(int)(Math.random()*3+1);
+				if(y[r]!=0){
+					y[r]--;
+					if(r==1)
+						s+=addLetter();
+					else if(r==2)
+						s+=addDigit();
+					else
+						s+=addSC();
+				}
+			}
+			s+="\r\n";
+			x[0]--;
+		}
+		return s;
+	}
+	public boolean control(int x[]){
+		for(int i=1;i<x.length;i++)
+			if(x[i]!=0)
+				return false;
+		return true;
+	}
+	public void copy(int x[],int y[]){
+		for(int i=0;i<x.length;i++)
+			y[i]=x[i];
+	}
+	public char addLetter(){
+		return 'A';
+	}
+	public char addDigit(){
+		return (char)((int)(Math.random()*(58-48)+48));
+	}
+	public char addSC(){
+		int x=(int)(Math.random()*4+1),min,max;
+		if(x==1){
+			min=33;
+			max=47;
+		}
+		else if(x==2){
+			min=58;
+			max=64;
+		}
+		else if(x==3){
+			min=91;
+			max=96;
+		}
+		else{
+			min=123;
+			max=126;
+		}
+		return (char)((int)(Math.random()*(max+1-min)+min));
 	}
 }
